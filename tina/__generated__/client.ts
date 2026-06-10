2d63b65abdca056db8100b5d4172e4b5d1e0749b
amandamwong <amanda_wong@alumni.utoronto.ca>
2026-06-09 22:09:36 -0400

    fix: tina lock file and build command

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '441271e3b12a865a9d1d9de44e0a1102e3bdbeee', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/amand/Documents/amanda-wong-site/tina/__generated__/.cache/1781057139458', url: 'https://content.tinajs.io/2.4/content/7e700257-effd-440b-aa89-2e2552040a2e/github/main', token: '441271e3b12a865a9d1d9de44e0a1102e3bdbeee', queries,  });
 export default client;
   
